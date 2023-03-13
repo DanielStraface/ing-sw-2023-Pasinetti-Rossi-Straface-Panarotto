e3bdf781b5b9b690e601204cb42b6add81451610
m-sole <mariasole.rossi@mail.polimi.it>
2023-03-13 11:38:59 +0100

    edited Item

--- src/main/java/it/polimi/ingsw/model/Item.java
+++ src/main/java/it/polimi/ingsw/model/Item.java
@@ -1,18 +1,7 @@
 package it.polimi.ingsw.model;
 
 class Item {
-    Category category;
-    Item(String type){
-        switch(type){
-            case "CAT" -> this.category = Category.CAT;
-            case "BOOK" -> this.category = Category.BOOK;
-            case "GAME" -> this.category = Category.GAME;
-            case "FRAME" -> this.category = Category.FRAME;
-            case "TROPHY" -> this.category = Category.TROPHY;
-            case "PLANT" -> this.category = Category.PLANT;
-        }
-    }
-    public void printCategoryType(){
-        System.out.println("This is a " + category);
-    }
+    private Category category;
+    private Category getCategoryType(){}
+
 }
