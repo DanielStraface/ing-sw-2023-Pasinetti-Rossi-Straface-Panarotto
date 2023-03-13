9db579627f9374f3d43989a757a9402366097877
C-Paso <10666028@polimi.it>
2023-03-13 10:56:29 +0100

    Edit Player

--- src/main/java/it/polimi/ingsw/model/Player.java
+++ src/main/java/it/polimi/ingsw/model/Player.java
@@ -2,9 +2,30 @@ package it.polimi.ingsw.model;
 
 import it.polimi.ingsw.model.Shelf;
 
+import java.util.List;
+
 class Player {
-    private String name;
+    private String nickname;
     private int score;
     private Shelf myShelf;
+    private PersonalObjCard myPersonalObjCard;
     private boolean isFirstPlayer;
-}
+    private int clientID;
+    private List<Item> selectItems;
+
+    public String getNickname() {}
+
+    public int getScore() {}
+
+    public PersonalObjCard getMyPersonalOBjCard() {}
+
+    public Shelf getMyShelf() {}
+
+    public boolean getFirstPlayer() {}
+
+    private List<Item> pickItems() {}
+
+    private void putItemInShelf() {}
+
+
+
