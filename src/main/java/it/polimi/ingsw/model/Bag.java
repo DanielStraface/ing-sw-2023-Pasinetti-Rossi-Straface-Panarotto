a4b6a6eb99af5885349ac78189edd5619d6e2182
DanielStraface <100609095+DanielStraface@users.noreply.github.com>
2023-03-15 11:48:03 +0100

    added constructor and get methods to bag,game and gameboard classes

--- src/main/java/it/polimi/ingsw/model/Bag.java
+++ src/main/java/it/polimi/ingsw/model/Bag.java
@@ -1,10 +1,21 @@
 package it.polimi.ingsw.model;
 
 import java.util.List;
+import java.util.Random;
 
 class Bag {
     private List<Item> itemCards;
 
 
-    public Item drawItem(){}
+    public Item drawItem(){
+        Item temp;
+        if(itemCards.size()>0){
+            Random random = new Random();
+            int randomNumber = random.nextInt(itemCards.size());
+            temp = itemCards.get(randomNumber);
+            itemCards.remove(randomNumber);
+            return temp;
+        }
+    }
+
 }
