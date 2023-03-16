2f0918be9b2baee69512f389ea22819ddb085494
C-Paso <10666028@polimi.it>
2023-03-16 22:54:25 +0100

    First Edit pickItems and putItem

--- src/main/java/it/polimi/ingsw/model/Player.java
+++ src/main/java/it/polimi/ingsw/model/Player.java
@@ -15,11 +15,11 @@ class Player {
     private int clientID;
     private List<Item> selectItems;
 
-    public Player(String nickname, int clientID, boolean isFirstPlayer) {
+    public Player(String nickname, int clientID, boolean isFirstPlayer) throws Exception {
         this.nickname = nickname;
         this.clientID = clientID;
+        this.score = 0;
         this.isFirstPlayer = false;
-        selectItems = new ArrayList<>();
 
         public String getNickname(){
             return this.nickname;
@@ -39,9 +39,58 @@ class Player {
         public boolean getFirstPlayer(){
             return this.isFirstPlayer;
         }
-        private List<Item> pickItems(){
+        private List<Item> pickItems(Coordinate[] selectedcoords, Item[][] gameGrid) throws Exception {
+            if (selectedcoords== null || selectedcoords.length==0){
+                throw new Exception("No item selected");
+            }
+            int x= selectedcoords[0].getX();
+            int y= selectedcoords[0].getY();
+
+            for(int i=1; i<selectedcoords.length; i++){
+                if(selectedcoords[i].getX()!=x && selectedcoords[i].getY()!=y) {
+                    throw new Exception("Invalid selection:different cols and rows");
+                }
+            }
+            for (int i = 0; i < selectedcoords.length; i++) {
+                Coordinate coord = selectedcoords[i];
+                int row = coord.getX();
+                int col = coord.getY();
+
+                if (gameGrid[row][col - 1] == null || gameGrid[row][col + 1] == null) {
+                    continue;
+                }
+                if (gameGrid[row - 1][col] == null || gameGrid[row + 1][col] == null) {
+                    continue;
+                }
+                throw new Exception("Invalid selection: no free sides");
+            }
+            selectItems = new ArrayList<Item>();
+            for (int i=0; i<selectedcoords.length; i++) {
+                int row = selectedcoords[i].getX();
+                int col = selectedcoords[i].getY();
+
+                selectItems[i]= gameGrid[row][col];
+                gameGrid[row][col]=null;
+            }
+            return selectItems;
         }
-        private void putItemInShelf(){
+        private void putItemInShelf(Item selectedItem, int selectedCol, Shelf[][] myShelf){
+            if (selectedCol >= 5) {
+                throw new IllegalArgumentException("selectedCol must be less than 5");
+            }
+            int lastRow = -1;
+            for (int row = 0; row<6; row++) {
+                if (myShelf[row][selectedCol] == null) {
+                    lastRow = row;
+                }
+            }
+            if (lastRow == -1) {
+                throw new IllegalStateException("Column " + selectedCol + " is full");
+            }
+            myShelf[lastRow][selectedCol] = selectedItem;
+
+            return myShelf;
+            }
         }
     }
 }
