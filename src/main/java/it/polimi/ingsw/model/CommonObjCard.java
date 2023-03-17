6258d70397fa2f33080ad65110e3cf202c9084dc
MatteoPana <matteo.panarotto@mail.polimi.it>
2023-03-17 17:08:29 +0100

    Full implementation of Json reading PersonalObjCard and first look CommonObjCard

--- src/main/java/it/polimi/ingsw/model/CommonObjCard.java
+++ src/main/java/it/polimi/ingsw/model/CommonObjCard.java
@@ -1,13 +1,48 @@
 package it.polimi.ingsw.model;
 
-class CommonObjCard {
+/**
+ * STATUS DESCRIPTION FOR RowsColumnsCard
+ * 6 --> Row checker
+ * 8 --> Column checker
+ */
+
+/**
+ * STATUS DESCRIPTION FOR GroupCards
+ *  1 ->
+ *  5 ->
+ *  7 ->
+ *  11 ->
+ */
+
+/** STATUS DESCRIPTION FOR ThreeDifferentTypes
+ *
+ * 4 --> RowsDifferentTypes
+ * 9 --> ColumnsDifferentTypes
+ */
+
+/**
+ * STATUS DESCRIPTION FOR CornerDiagonals
+ * 2 -->
+ * 3 -->
+ * 12 -->
+ */
+
+/**
+ * STATUS DESCRIPTION FOR XCard
+ * 12 -->
+ */
+
+public class CommonObjCard {
     private static final int DIM = 4;
     private int[] objPoints;
     private int nextPoints;
     private String commonObjCardDescription; // See JSON reading from file with Java
 
+    protected int status;
+
     /**
-     * Constructor for CommonObjCard
+     * Constructor for CommonObjCard. it is based on how many players will play the game because the points provides by
+     * CommonObjCard instance is different.
      */
     public CommonObjCard(int numberOfPlayers){
         switch(numberOfPlayers){
@@ -35,6 +70,7 @@ class CommonObjCard {
 
     /* METHOD SECTION */
 
+    /* -- get methods */
     /**
      * Method getCommonObjCardDescription returns a string with the description of this card
      * -- Maybe this method must be edited in function of JSON loading strings
@@ -52,27 +88,143 @@ class CommonObjCard {
         return tempPoint;
     }
 
-    public boolean check(Shelf shelf){return false;}
+    /* -- logic methods */
+    public boolean check(Player player){return false;}
 }
 
-class ColumnChecker extends CommonObjCard {
-    public ColumnChecker(int numberOfPlayers){
+class RowsColumnsCard extends CommonObjCard {
+    public RowsColumnsCard(int numberOfPlayers, int status){
         super(numberOfPlayers);
+        this.status = status;
     }
 
-    @Override
-    public boolean check(Shelf shelf){
+    public boolean checker(){
+        boolean result = false;
+        switch (status){
+            case 6 -> result = rowsChecker();
+            case 8 -> result = columnsChecker();
+            default -> {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean rowsChecker(){
+        return false;
+    }
+
+    private boolean columnsChecker(){
         return false;
     }
 }
 
-class RowChecker extends CommonObjCard {
-    public RowChecker(int numberOfPlayers){
+
+
+class ThreeDifferentTypes extends CommonObjCard {
+    public ThreeDifferentTypes(int numberOfPlayers, int status){
         super(numberOfPlayers);
+        this.status = status;
     }
 
-    @Override
-    public boolean check(Shelf shelf){
+    public boolean checker(){
+        boolean result = false;
+        switch (status) {
+            case 4 -> result = rowsDifferentTypes();
+            case 9 -> result = columnsDifferentTypes();
+            default -> {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean rowsDifferentTypes(){
         return false;
+    }
+
+    private boolean columnsDifferentTypes(){
+        return false;
+    }
+}
+
+class CornerDiagonals extends CommonObjCard {
+    public CornerDiagonals(int numberOfPlayers, int status){
+        super(numberOfPlayers);
+        this.status = status;
+    }
+
+    public boolean checker(){
+        boolean result = false;
+        switch (status) {
+            case 2 -> result =diagonals();
+            case 3 -> result = corners();
+            case 12 -> result = descMatrix();
+            default -> {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean diagonals(){
+        return false;
+    }
+
+    public boolean corners(){
+        return false;
+    }
+
+    public boolean descMatrix() {
+        return false;
+    }
+}
+
+class GroupCards extends CommonObjCard {
+    public GroupCards(int numberOfPlayers, int status){
+        super(numberOfPlayers);
+        this.status = status;
+    }
+
+    public boolean check(){
+        boolean result = false;
+        switch (status) {
+            case 1 -> result =groupOfTwo();
+            case 5 -> result = groupOfFour();
+            case 7 -> result = groupOfSquares();
+            case 11 -> result = groupOfEight();
+            default -> {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean groupOfTwo(){
+        return true;
+    }
+
+    private boolean groupOfFour(){
+        return true;
+    }
+
+    private boolean groupOfSquares(){
+        return true;
+    }
+
+    private boolean groupOfEight(){
+        return true;
+    }
+
+}
+
+class XCards extends CommonObjCard{
+    public XCards(int numberOfPlayers, int status){
+        super(numberOfPlayers);
+        this.status = status;
+    }
+
+    public boolean xChecker(){
+        return true;
     }
 }
--- src/main/java/it/polimi/ingsw/model/PersonalObjCard.java
+++ src/main/java/it/polimi/ingsw/model/PersonalObjCard.java
@@ -1,39 +1,73 @@
 package it.polimi.ingsw.model;
 
-class PersonalObjCard {
+public class PersonalObjCard {
 
     /* ATTRIBUTES SECTIONS */
-    private String personalObjCardDescription; // The name of the personalObjCard
-    private Item[][] cardGrid; // This provides the position of item needed to score points
+    private String personalObjCardDescription; // The name of the personalObjCard, initially store in JSON file
+    private Item[][] cardGrid; // This provides the position of item needed to score points, initially store in JSON file
+
 
     /* METHODS SECTION */
 
+    /* -- get methods -- */
     /**
      * Method getPersonalObjCardDescription returns a string with the description of this card.
-     * -- Maybe this method must be edit in function of JSON loading strings
+     * @return this.personalObjCardDescription
      */
     public String getPersonalObjCardDescription() {
-        return personalObjCardDescription;
+        return this.personalObjCardDescription;
     }
 
     /**
+     * Method getCardGrid returns a reference to cardGrid.
+     * @return this.cardGrid
+     */
+    public Item[][] getCardGrid(){
+        return this.cardGrid;
+    }
+
+    /* -- logic methods -- */
+    /**
      * Method goalReached returns true if there is a match between the item position in the player shelf and in the
-     * template grid of the card. This grid is load from a Json file.
+     * template grid of the card. This grid was loaded from a Json file.
+     * @return TRUE <==> (forall Item I1 in the (x,y) position in player's myShelf
+     *                      exists Item I2 in the (x,y) position in cardGrid template) &&
+     *                      I1.getCategoryType() == I2.getCategoryType()
      */
     public boolean goalReached(Shelf shelf) {
-        /* Two for cycle to analyse both matrix in player shelf and template grid */
+        /* Two for-cycle to analyse both matrix in player shelf and template grid */
         for (int i = 0; i < cardGrid.length; i++) {
             for (int j = 0; j < cardGrid[i].length; j++) {
-                /* if the position in the template grid is not null there is an item type */
-                if (cardGrid[i][j] != null) {
+                /* if the position in the template grid and player's shelf is not null there is an item type */
+                if (cardGrid[i][j].getCategoryType() != null && shelf.GetShelfGrid()[i][j].getCategoryType() != null) {
                     /* If there is at least one type of item that is not equal to the item in the player shelf
-                    *  the goal is not reached. The method returns false */
-                    if (cardGrid[i][i].getCategoryType() != shelf.GetShelfGrid()[i][j].getCategoryType()) {
+                     *  the goal is not reached. The method returns false */
+                    /** ACCESSORY CODE HERE **/
+                    System.out.println(cardGrid[i][j].getCategoryType() + " =?= " +
+                                    shelf.GetShelfGrid()[i][j].getCategoryType()
+                            );
+                    /*************************/
+                    if(cardGrid[i][j].getCategoryType() != shelf.GetShelfGrid()[i][j].getCategoryType()){
                         return false;
                     }
                 }
             }
         }
+        /* If during the cardGrid scanning the condition of @IF1 is always false it means that all the matches
+        *  between the two item grid are true. The method must return true */
         return true;
+    }
+
+    /** ACESSORY METHOD HERE **/
+    public void show(){
+        System.out.println(this.getPersonalObjCardDescription());
+        System.out.println("This card has a " + cardGrid.length + " card grid height");
+        System.out.println("This card has a " + cardGrid[0].length + " card grid width");
+        for(int i=0;i<this.cardGrid.length;i++){
+            for(int j=0;j<this.cardGrid[0].length;j++){
+                System.out.print(this.cardGrid[i][j].getCategoryType() + " ");
+            }
+            System.out.println();
+        }
     }
 }
