282bae13a87e3819893428b4de5a1e41a30c3e82
DanielStraface <100609095+DanielStraface@users.noreply.github.com>
2023-03-18 21:02:20 +0100

    added rowsDifferentTypes method (4)

--- src/main/java/it/polimi/ingsw/model/ThreeDifferentTypes.java
+++ src/main/java/it/polimi/ingsw/model/ThreeDifferentTypes.java
@@ -1,5 +1,7 @@
 package it.polimi.ingsw.model;
 
+import java.util.HashSet;
+
 class ThreeDifferentTypes extends CommonObjCard {
     public ThreeDifferentTypes(int numberOfPlayers, int status) {
         super(numberOfPlayers);
@@ -19,6 +21,26 @@ class ThreeDifferentTypes extends CommonObjCard {
     }
 
     private boolean rowsDifferentTypes() {
+        Item[][] grid = player.getMyShelf().GetShelfGrid();
+        int rowCounter = 0;
+        for(int i=0; i<6; i++) {
+            HashSet<Category> uniqueObjects = new HashSet<Category>();
+            for (int j = 0; j < 5; j++) {
+                if (grid[i][j].getCategoryType() == null) {
+                    j = 5;
+                }
+                else {
+                    Category scanned = grid[i][j].getCategoryType();
+                    uniqueObjects.add(scanned);
+                }
+            }
+            if (uniqueObjects.size() <= 3) {
+                rowCounter++;
+            }
+        }
+        if(rowCounter>=4){
+            return true;
+        }
         return false;
     }
 
