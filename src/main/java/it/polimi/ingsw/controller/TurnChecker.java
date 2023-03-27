5d988ca0cf4edbe9a5c978d85a2979f30c01ec6c
DanielStraface <100609095+DanielStraface@users.noreply.github.com>
2023-03-27 11:50:23 +0200

    Created TurnChecker class

---
+++ src/main/java/it/polimi/ingsw/controller/TurnChecker.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw.controller;
+
+public class TurnChecker {
+
+    private void commonObjCardCheck(){}
+    private void personalObjCardCheck(){}
+    private void refillGameBoardCheck(){}
+    private void lastTurnCheck(){}
+
+}
