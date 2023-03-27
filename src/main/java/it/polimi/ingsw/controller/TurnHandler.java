e69091477ffe13093735455acbd334c747a0d418
m-sole <mariasole.rossi@mail.polimi.it>
2023-03-27 12:23:58 +0200

    modified TurnHandler class

--- src/main/java/it/polimi/ingsw/controller/TurnHandler.java
+++ src/main/java/it/polimi/ingsw/controller/TurnHandler.java
@@ -2,6 +2,9 @@ package it.polimi.ingsw.controller;
 
 public class TurnHandler {
     private TurnChecker turnChecker;
+    public TurnHandler(){
+        turnChecker= new TurnChecker();
+    }
 
     public void nextTurn(){};
     public void manageTurn(){};
