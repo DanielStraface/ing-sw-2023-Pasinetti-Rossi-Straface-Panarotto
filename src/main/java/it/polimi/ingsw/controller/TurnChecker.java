9a6b780bbe34b2c4478a8765df8b542fa99c6ff9
DanielStraface <100609095+DanielStraface@users.noreply.github.com>
2023-03-28 19:15:06 +0200

    Added refillGameBoardCheck, lastTurnCheck (with comments)

--- src/main/java/it/polimi/ingsw/controller/TurnChecker.java
+++ src/main/java/it/polimi/ingsw/controller/TurnChecker.java
@@ -2,30 +2,112 @@ package it.polimi.ingsw.controller;
 
 import it.polimi.ingsw.model.Game;
 import it.polimi.ingsw.model.Player;
+import it.polimi.ingsw.model.Shelf;
 import it.polimi.ingsw.model.comcard.CommonObjCard;
-import it.polimi.ingsw.model.personcard.PersonalObjCard;
+
 
 public class TurnChecker {
 
-    private CommonObjCard commonObjCard;
-    private PersonalObjCard personalObjCard;
+    private static final int SHELF_ROWS=6;
+    private static final int SHELF_COLUMNS=5;
+    private static final int OCCUPIED = 2;
+
+
+
 
     public boolean manageCheck(Player player, Game game){
         boolean isLastTurn;
         commonObjCardCheck(player, game);
-        isLastTurn = lastTurnCheck();
-        refillGameBoardCheck();
+        isLastTurn = lastTurnCheck(player);
+        refillGameBoardCheck(game);
         return isLastTurn;
     }
 
     private void commonObjCardCheck(Player player,Game game){
+        CommonObjCard commonObjCard;
+
+        // check if the current player has reached the goal for both CommonObjectiveCards in Game
         for(int i=0; i<game.getCommonObjCard().size(); i++){
             commonObjCard = game.getCommonObjCard().get(i);
             commonObjCard.doCheck(player);
         }
     }
 
-    private void refillGameBoardCheck(){}
-    private boolean lastTurnCheck(){return false;}
+    private void refillGameBoardCheck(Game game){
+        boolean check = true;
+        int[][] validGrid = game.getValidGrid();
+
+        /** check for every row and column (except first and last): if the slot is occupied by an Item,
+         *  check if any of its adjacent places are occupied by an Item as well.
+         *  If there's a single adjacent item, there's no need to refill the GameBoard and the check is
+         *  set to false. */
+        for(int i=1; i<8; i++){
+            for(int j=1; j<8; j++){
+                if(validGrid[i][j] == OCCUPIED){
+                    if(validGrid[i-1][j] == OCCUPIED ||
+                       validGrid[i][j-1] == OCCUPIED ||
+                       validGrid[i][j+1] == OCCUPIED ||
+                       validGrid[i+1][j] == OCCUPIED){
+                        check = false;
+                    }
+                }
+            }
+        }
+
+        /** these last two for cycles are used to check slots 3 and 4 for the first row and last column,
+         *  slots 4 and 5 for the first column and last row */
+        for(int i=3; i<5; i++){
+            if(validGrid[0][i] == OCCUPIED){
+                if(validGrid[1][i] == OCCUPIED ||
+                   validGrid[0][i+1] == OCCUPIED ||
+                   validGrid[0][i-1] == OCCUPIED){
+                    check = false;
+                }
+            }
+            if(validGrid[i][8] == OCCUPIED){
+                if(validGrid[i][7] == OCCUPIED ||
+                   validGrid[i-1][8] == OCCUPIED ||
+                   validGrid[i+1][8] == OCCUPIED){
+                    check = false;
+                }
+            }
+        }
+        for(int i=4; i<6; i++){
+            if(validGrid[i][0] == OCCUPIED){
+                if(validGrid[i][1] == OCCUPIED ||
+                   validGrid[i-1][0] == OCCUPIED ||
+                   validGrid[i+1][0] == OCCUPIED){
+                    check = false;
+                }
+            }
+            if(validGrid[8][i] == OCCUPIED){
+                if(validGrid[7][i] == OCCUPIED ||
+                   validGrid[8][i-1] == OCCUPIED ||
+                   validGrid[8][i+1] == OCCUPIED){
+                    check = false;
+                }
+            }
+        }
+
+        //if no Item has adjacent Items, the GameBoard needs to be refilled
+        if(check == true){
+            game.refillGameBoard();
+        }
+    }
+
+    private boolean lastTurnCheck(Player player){
+        Shelf playerShelf = player.getMyShelf();
+
+        /** if the current player's shelf is full, the last turn is triggered by this method
+         *  returning a true boolean */
+        for(int i=0; i<SHELF_ROWS; i++){
+            for(int j=0; j<SHELF_COLUMNS; j++){
+                if(playerShelf.getShelfGrid()[i][j].getCategoryType() == null){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
 }
