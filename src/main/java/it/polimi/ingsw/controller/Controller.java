5918cb9cbd95e26b311a59a73e1e867fc784ed6b
C-Paso <10666028@polimi.it>
2023-05-17 13:10:30 +0200

    Added checks about player's choices in Controller.

--- src/main/java/it/polimi/ingsw/controller/Controller.java
+++ src/main/java/it/polimi/ingsw/controller/Controller.java
@@ -1,8 +1,10 @@
 package it.polimi.ingsw.controller;
 
 import it.polimi.ingsw.distributed.Client;
-import it.polimi.ingsw.model.Game;
-import it.polimi.ingsw.model.Player;
+import it.polimi.ingsw.exceptions.*;
+import it.polimi.ingsw.model.*;
+import it.polimi.ingsw.modelview.GameBoardView;
+import it.polimi.ingsw.modelview.ShelfView;
 
 import java.io.*;
 import java.rmi.RemoteException;
@@ -22,8 +24,12 @@ public class Controller {
     /* ATTRIBUTES SECTION */
     private int matchID;
     private Game game;
+    private int[][] validGrid;
     private final List<Client> clients = new ArrayList<>();
     private TurnHandler turnHandler;
+    private static final int INVALID = 0;
+    private static final int PLAYABLE = 1;
+    private static final int OCCUPIED = 2;
     private static final String SelectionError = "Try again, invalid selection due to: ";
 
     /* METHODS SECTION */
@@ -31,6 +37,7 @@ public class Controller {
     /* -- constructor --*/
     public Controller(Game game) {
         this.game = game;
+        this.validGrid=game.getGameboard().getValidGrid();
         turnHandler = new TurnHandler(game);
     }
 
@@ -64,7 +71,7 @@ public class Controller {
      * saveGame method saves the state of the game in a file
      * @param game - the current model of the match
      * @param fileName - the name of the saving file
-     * @author Christian Pasinetti
+     * @author Cristian Pasinetti
      */
     public static void saveGame(Game game, String fileName) {
         try{
@@ -83,7 +90,7 @@ public class Controller {
      * loadGame method loads a saved game from a file
      * @param fileName - the name of the file in which the game was saved
      * @return the game instance that represent the model stored in the fileName
-     * @author Christian Pasinetti
+     * @author Cristian Pasinetti
      */
     public static Game loadGame(String fileName) throws FileNotFoundException {
         try {
@@ -123,9 +130,126 @@ public class Controller {
     public boolean getGameOver(){return this.turnHandler.getGameOver();}
     public int getMatchID() {return this.matchID;}
 
+    private void noItemSelectedChecker(List<int[]> coords) throws InvalidSelectionException {
+        for(int i=0;i<coords.size();i++){
+            if(validGrid[coords.get(i)[0]][coords.get(i)[1]] == INVALID)
+                throw new SelectionInvalidOrEmptySlotException("selected item in invalid slot");
+            else if(validGrid[coords.get(i)[0]][coords.get(i)[1]] == PLAYABLE )
+                throw new SelectionInvalidOrEmptySlotException("selected item in empty slot");
+        }
+    }
+    private void freeSideChecker(List<int[]> coords) throws InvalidSelectionException {
+        for (int i = 0; i < coords.size(); i++) {
+            final int FIRST_ROW = 0;
+            final int LAST_ROW = 8;
+            final int FIRST_COLUMN = 0;
+            final int LAST_COLUMN = 8;
+            int row = coords.get(i)[0];
+            int col = coords.get(i)[1];
+            /* if items are on the edge of the game board they have always almost a free side  */
+            if (col != FIRST_COLUMN && col != LAST_COLUMN && row != FIRST_ROW && row != LAST_ROW) {
+                if (validGrid[row][col - 1] == OCCUPIED && validGrid[row][col + 1] == OCCUPIED &&
+                        validGrid[row - 1][col] == OCCUPIED && validGrid[row + 1][col] == OCCUPIED)
+                    throw new NoFreeSidesException("selected item with no free sides");
+            }
+        }
+    }
+    private void rowAndColChecker(List<int[]> coords) throws InvalidSelectionException {
+        boolean sameX = true;
+        boolean sameY = true;
+        int XOfFirstCoordinate = coords.get(0)[0];
+        int YOfFirstCoordinate = coords.get(0)[1];
+        /* For-cycle to analyse values of coordinates: first bond: Items from the same row or column*/
+        for (int i = 1; i < coords.size(); i++) {
+            /*If all coordinates haven't the same x: items will not be picked from the same row */
+            if (coords.get(i)[0] != XOfFirstCoordinate) {
+                sameX = false;
+            }
+            /* If all coordinates haven't the same y: items will not be picked from the same column */
+            if (coords.get(i)[1] != YOfFirstCoordinate) {
+                sameY = false;
+            }
+        }
+        /* If all coordinates haven't the same x or the same y: items can not be picked from the game board */
+        if (!sameX && !sameY) {
+            throw new NotSameRowOrColException("no same rows or cols selection");
+        }
+        boolean consecutiveX = true;
+        boolean consecutiveY = true;
+        if (sameX) {
+            int minY = coords.get(0)[1];
+            int maxY = coords.get(0)[1];
+
+            for (int i = 1; i < coords.size(); i++) {
+                if (coords.get(i)[1] < minY) {
+                    minY = coords.get(i)[1];
+                }
+            }
+            for (int i = 1; i < coords.size(); i++) {
+                if (coords.get(i)[1] > maxY) {
+                    maxY = coords.get(i)[1];
+                }
+            }
+            if (coords.size() == 3) {
+                if (maxY - minY != 2) {
+                    consecutiveY = false;
+                }
+            }
+            if (coords.size() == 2) {
+                if (maxY - minY != 1) {
+                    consecutiveY = false;
+                }
+            }
+            if (!consecutiveY) {
+                throw new NoConsecutiveSelectionException("no consecutive items selection");
+            }
+        }
+
+        if (sameY) {
+            int minX = coords.get(0)[0];
+            int maxX = coords.get(0)[0];
+
+            for (int i = 1; i < coords.size(); i++) {
+                if (coords.get(i)[0] < minX) {
+                    minX = coords.get(i)[0];
+                }
+            }
+            for (int i = 1; i < coords.size(); i++) {
+                if (coords.get(i)[0] > maxX) {
+                    maxX = coords.get(i)[0];
+                }
+            }
+            if (coords.size() == 3) {
+                if (maxX - minX != 2) {
+                    consecutiveX = false;
+                }
+            }
+            if (coords.size() == 2) {
+                if (maxX - minX != 1) {
+                    consecutiveX = false;
+                }
+            }
+            if (!consecutiveX) {
+                throw new NoConsecutiveSelectionException("no consecutive item selection");
+            }
+        }
+    }
+    private void columnCheck(int columnSelected) throws FullColumnException {
+        int[] lastRows = shelfView.getLastRow();
+        if(numOfItems > lastRows[columnSelected]+1){
+            throw new FullColumnException();
+        }
+    }
+    public void selectionChecker(List<int[]> coords, int col) throws InvalidSelectionException, FullColumnException {
+        noItemSelectedChecker(coords);
+        freeSideChecker(coords);
+        rowAndColChecker(coords);
+        columnCheck(col);
+    }
+
     /* update methods */
 
-    public void update(Client o, List<int[]> coords, Integer column) throws RemoteException {
+    public void update(Client o,List<int[]> coords, Integer column) throws RemoteException{
         boolean fromValidClient = false;
         for(Client c : this.clients){
             if(c.getClientID() == o.getClientID())
@@ -135,6 +259,11 @@ public class Controller {
             System.err.println("Match#" + this.getMatchID() + " discarding notification from client with "
                     + o.getClientID() + " clientID number in update");
         } else {
+                try {
+                    selectionChecker(coords, column);
+                } catch (InvalidSelectionException | FullColumnException e) {
+                    throw new RuntimeException(e);
+                }
             game.getCurrentPlayer().pickItems(coords, game.getGameboard().getGameGrid(),
                     game.getGameboard().getValidGrid());
             game.getCurrentPlayer().putItemInShelf(column);
--- src/main/java/it/polimi/ingsw/view/commands/SelectItemsCommand.java
+++ src/main/java/it/polimi/ingsw/view/commands/SelectItemsCommand.java
@@ -3,7 +3,6 @@ package it.polimi.ingsw.view.commands;
 import it.polimi.ingsw.modelview.GameBoardView;
 
 import it.polimi.ingsw.exceptions.*;
-import it.polimi.ingsw.modelview.GameBoardView;
 
 import java.util.List;
 import java.util.Scanner;
