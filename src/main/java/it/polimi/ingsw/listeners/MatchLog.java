a359f5c3d839a615d10b19d5d5db5fd565310434
DanielStraface <100609095+DanielStraface@users.noreply.github.com>
2023-05-28 13:26:57 +0200

    Added more javadoc comments and fixed a typo

--- src/main/java/it/polimi/ingsw/listeners/MatchLog.java
+++ src/main/java/it/polimi/ingsw/listeners/MatchLog.java
@@ -14,11 +14,21 @@ public class MatchLog implements Client {
     private int listenerID;
     private int turnCounter;
 
+    /**
+     * Constructor method
+     * @param matchID int -> match's ID
+     */
     public MatchLog(int matchID){
         this.turnCounter = 1;
         this.matchID = matchID;
         this.nickname = "MatchLog for match#" + this.matchID;
     }
+
+    /**
+     * Update method passing a GameView to save the match through the MatchLog
+     * @param game GameView
+     * @throws RemoteException
+     */
     @Override
     public void update(GameView game) throws RemoteException {
         System.out.println("Log := match#" + matchID + ", turn n. " + turnCounter + " : " +
@@ -27,27 +37,54 @@ public class MatchLog implements Client {
         turnCounter++;
     }
 
+    /**
+     * Update method passing a message String to save the match through the MatchLog
+     * @param msg String
+     * @throws RemoteException
+     */
     @Override
     public void update(String msg) throws RemoteException {
         if(msg.equals("START GAME")) return;
         System.out.println("Log := match#" + matchID + " new clients string message notification : \n->" + msg);
     }
 
+    /**
+     * Update method passing a clientID to save the match through the MatchLog
+     * @param clientID int
+     * @throws RemoteException
+     */
     @Override
     public void update(int clientID) throws RemoteException {
         this.listenerID = clientID;
     }
 
+    /**
+     * Get method for a player's nickname String
+     * @return nickname String
+     * @throws RemoteException
+     */
     @Override
     public String getNickname() throws RemoteException {
         return this.nickname;
     }
 
+    /**
+     * Get method for a player's clientID
+     * @return int -> clientID
+     * @throws RemoteException
+     */
     @Override
     public int getClientID() throws RemoteException {
         return this.listenerID;
     }
 
+    /**
+     * Update method passing a client and all his choices made to save the match through the MatchLog
+     * @param client the client making the choices
+     * @param coords the item's coordinates chosen
+     * @param column the shelf's column choice
+     * @throws RemoteException
+     */
     public void update(Client client, List<int[]> coords, Integer column) throws RemoteException {
         System.out.println("Log := match#" + matchID + ", user player " + client.getClientID() +
                 " has taken and reorder from the gameboard the following items (in coords):");
